3b1c379efb175679ec0c4ce7195f2521e1d3788a
Kerenza13 <carlichesjunior@gmail.com>
2024-11-20 19:46:28 +0100

    20-11-2024

---
+++ 01-dom-reservas/src/main.js
@@ -0,0 +1,30 @@
+/**
+ * @author Carlos Morillas Delgado
+ * @description
+ */
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    // elemento de entrada a mi applicacion
+    const app = document.getElementById('app');
+    // creo elemento h1
+    const h1 = document.createElement("h1");
+    h1.textContent = "Hello World!";
+    app.appendChild(h1);
+    // creo un div
+    const divCard = documen.createElement("div");
+    divCard.id = "card";
+    divCard.classList.add("principal card");
+    // creo un parrafo
+    const p = document.createElement("p");
+    p.textContent = "Hola soy un parrafo";
+    divCard.appendChild(p);
+    // creo un boton
+    const mensajeBtn = document.createElement("button");
+    mensajeBtn.textContent = "Mostrar Mensaje";
+    divCard.appendChild(mensajeBtn);
+
+    app.appendChild(divCard)
+
+})
